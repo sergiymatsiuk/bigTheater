6eb819917c62772cd8330887c0b1cf920b3b109b
sergiymatsiuk <sergiymatsiuk1997@gmail.com>
2021-06-03 17:18:39 +0300

    second homework

---
+++ index.js
@@ -0,0 +1,20 @@
+const swiper =new Swiper('.partners-logo', {
+    loop: true,
+    loopedSlides: 4,
+    centeredSlides: false,
+    initialSlide: 4,
+    slideToClickedSlide: true,
+    grabCursor: true,
+    slidesPerView: 'auto',
+
+    breakpoints: {
+        1400: {
+            slidesPerView: 4,
+            spaceBetween: 30
+        }
+    }
+})
+
+const bigTheaterSlider =new Swiper('.big-theater-slider-container', {
+    loop: true,
+})
